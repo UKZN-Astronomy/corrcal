e1bdcdfb1950da6e3cb1e5efadf086fbee998e86
ronniyjoseph <ronniyjoseph@gmail.com>
2019-07-26 17:11:26 +0800

    Calculate gradient first before fitting gain for debugging

--- corrcal2_example.py
+++ corrcal2_example.py
@@ -27,18 +27,18 @@ mat = corrcal2.read_sparse('signal_sparse2_test.dat')
 # if you want to test timings, you can do so here.  Set t_min to some length of
 # time, and code will see how many gradient operations it can get through
 # during at least t_min seconds
-niter = 0;
-t_min = 1e-4
-t1 = time.time()
-while (time.time() - t1) < t_min:
-    grad = corrcal2.get_gradient(gvec, data, mat, ant1, ant2)
-    niter = niter + 1
-t2 = time.time()
-nant = gvec.size / 2
-# time per gradient
-print( 'average time was ' + repr((time.time() - t1) / niter))
-# time (in microseconds) per visibility
-print( 'scaled_time was ' + repr((t2 - t1) / niter / nant / (nant - 1) * 1e6))
+# niter = 0;
+# t_min = 1e-4
+# t1 = time.time()
+# while (time.time() - t1) < t_min:
+#     grad = corrcal2.get_gradient(gvec, data, mat, ant1, ant2)
+#     niter = niter + 1
+# t2 = time.time()
+# nant = gvec.size / 2
+# # time per gradient
+# print( 'average time was ' + repr((time.time() - t1) / niter))
+# # time (in microseconds) per visibility
+# print( 'scaled_time was ' + repr((t2 - t1) / niter / nant / (nant - 1) * 1e6))
 
 # scipy nonlinear conjugate gradient seems to work pretty well.
 # note that it can use overly large step sizes in trials causing
@@ -50,9 +50,14 @@ from scipy.optimize import fmin_cg
 
 fac = 1000.0;
 t1 = time.time()
-asdf = fmin_cg(corrcal2.get_chisq, gvec * fac, corrcal2.get_gradient, (data, mat, ant1, ant2, fac))
-t2 = time.time()
-print( 'elapsed time to do nonlinear fit for ' + repr(nant) + ' antennas was ' + repr(t2 - t1))
-fit_gains = asdf / fac
-plt.plot(data[1::2] ** 2 + data[::2] ** 2)
-plt.show()
+
+
+print(data.shape)
+corrcal2.get_chisq(gvec * fac, data, mat, ant1, ant2, scale_fac=fac)
+corrcal2.get_gradient(gvec*fac, data, mat, ant1, ant2, fac)
+#asdf = fmin_cg(corrcal2.get_chisq, gvec * fac, corrcal2.get_gradient, (data, mat, ant1, ant2, fac))
+# t2 = time.time()
+# print( 'elapsed time to do nonlinear fit for ' + repr(nant) + ' antennas was ' + repr(t2 - t1))
+# #fit_gains = asdf / fac
+# #plt.plot(data[1::2] ** 2 + data[::2] ** 2)
+# #plt.show()
