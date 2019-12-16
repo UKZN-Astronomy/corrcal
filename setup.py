76ed14c7ab6025c6fd89a3cffc263e58d6f3e317
piyanat <piyanat.kittiwisit@gmail.com>
2019-12-16 14:57:51 -0700

    Use CFFI extension builder

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-from setuptools import setup, find_packages, Extension
+import os
+from setuptools import setup
+from setuptools import find_packages
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+pkgdir = os.path.dirname(os.path.abspath(__file__))
+
 setup(
     name="corrcal",
-    version="0.0.1",
+    version="0.0.2",
+    license='BSD 2-Clause License',
     author="CorrCal Development Team",
     description="Correlation Calibration for Quasi-redundant radio interferometer",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/UKZN-Astronomy/corrcal",
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD 2-Clause License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
-    ext_modules=[
-        Extension(
-            'libcorrcal',
-            sources=['corrcal/src/_corrcal.c'],
-            include_dirs=['corrcal/src'], install_requires=['numpy', 'cffi']
-        )
-    ]
+    install_requires=[
+        'numpy',
+        'cffi>=1.0'
+    ],
+    setup_requires=['cffi>=1.0'],
+    cffi_modules=["{pkgdir}/build_cffi.py:ffi".format(pkgdir=pkgdir)]
 )
